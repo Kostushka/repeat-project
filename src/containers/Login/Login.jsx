cd2d5b6ffa1ddea2fbebcd6db8051144e52c3d3a
Kostushka <nastyatery47@yandex.ru>
2022-09-04 23:16:43 +0300

    Added formik: LoginForm

--- src/containers/Login/Login.jsx
+++ src/containers/Login/Login.jsx
@@ -1,5 +1,53 @@
+import { useFormik } from 'formik';
+import styles from './Login.module.css';
+
 const Login = () => {
-    return <>Логин</>;
+    const formik = useFormik({
+        initialValues: {
+            login: '',
+            password: '',
+            toggle: false,
+        },
+        onSubmit: (values) => {
+            console.log(values);
+        },
+    });
+
+    return (
+        <form onSubmit={formik.handleSubmit} className={styles.block}>
+            <div>
+                <label htmlFor='login'>Логин</label>
+                <input
+                    placeholder='введите логин'
+                    name='login'
+                    type='text'
+                    onChange={formik.handleChange}
+                    value={formik.values.login}
+                />
+            </div>
+            <div>
+                <label htmlFor='password'>Пароль</label>
+                <input
+                    placeholder='введите пароль'
+                    name='password'
+                    type='password'
+                    onChange={formik.handleChange}
+                    value={formik.values.password}
+                />
+            </div>
+            <div>
+                <label htmlFor='toggle'>Запомнить меня</label>
+                <input
+                    name='toggle'
+                    type='checkbox'
+                    value={formik.values.toggle}
+                    onChange={formik.handleChange}
+                />
+            </div>
+
+            <button type='submit'>Отправить</button>
+        </form>
+    );
 };
 
 export default Login;
